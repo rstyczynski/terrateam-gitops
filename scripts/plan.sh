1e9026fc9113838fc100f863b5562d9ed0aa9bb5
Ryszard Styczynski <rstyczynski@gmail.com>
2025-08-20 19:38:21 +0200

    .

--- scripts/plan.sh
+++ scripts/plan.sh
@@ -12,7 +12,7 @@ echo "Creating Terraform execution plan..."
 # - Generate plan output
 # - Validate plan results
 
-echo Writes the plan to $1
+echo "Writes the plan to $1"
 
 echo "here is a plan line no.1" > $1
 echo "here is a plan line no.2" >> $1
