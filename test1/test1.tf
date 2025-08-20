3933bbfdde5c214fc8aff9785c5a3455f9c2cd3f
Ryszard Styczynski <rstyczynski@gmail.com>
2025-08-20 19:00:55 +0200

    .

---
+++ test1/test1.tf
@@ -0,0 +1,14 @@
+provider "null" {
+  version = "~> 3.0"
+}
+
+resource "null_resource" "example1" {
+  triggers = {
+    # This resource will be recreated whenever the value of this trigger changes.
+    trigger_key = "trigger_value4"
+  }
+
+  provisioner "local-exec" {
+    command = "echo This is a local provisioner nr.4"
+  }
+}
