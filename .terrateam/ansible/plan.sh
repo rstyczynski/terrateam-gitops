c56a89327de0da38c7dcd9963db4f0e316e6d9e6
Ryszard Styczynski <rstyczynski@gmail.com>
2025-08-21 10:25:41 +0200

    scripts updated with stderr and correct msg

--- .terrateam/ansible/plan.sh
+++ .terrateam/ansible/plan.sh
@@ -5,9 +5,9 @@ PLAN_FILE=$1
 echo "⚠️ ================================================" >&2
 echo "START: Ansible plan stage" >&2
 
-echo "TERRATEAM_PLAN_FILE (exported): $TERRATEAM_PLAN_FILE"
-echo "TERRATEAM_PLAN_FILE (argumnet): $PLAN_FILE"
-echo "TODO" > $PLAN_FILE
+echo "TERRATEAM_PLAN_FILE (exported): $TERRATEAM_PLAN_FILE" >&2
+echo "TERRATEAM_PLAN_FILE (argumnet): $PLAN_FILE" >&2
+echo "TODO Ansible plan" > $PLAN_FILE
 EXIT_CODE=0
 
 echo "END: Ansible plan stage" >&2
