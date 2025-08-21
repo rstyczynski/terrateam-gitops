9c9d98667220e8c90ecfb7aa60956726ee1134eb
Ryszard Styczynski <rstyczynski@gmail.com>
2025-08-21 07:59:36 +0200

    scripts fixed to just log

--- .terrateam/ansible/outputs.sh
+++ .terrateam/ansible/outputs.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+echo "⚠️ ================================================"
+echo "START: Ansible outputs stage"
 
 echo '{"status": "success", "stage": "outputs", "message": "Outputs stage completed successfully."}'
+EXIT_CODE=0
+
+echo "END: Ansible outputs stage"
+echo "⚠️ ================================================"
+exit $EXIT_CODE
+
+
