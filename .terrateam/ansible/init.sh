8b7ab3c25d2cbbc2098ad66f9639f54e5acb0935
Ryszard Styczynski <rstyczynski@gmail.com>
2025-08-21 08:13:30 +0200

    ansible CLI checks

--- .terrateam/ansible/init.sh
+++ .terrateam/ansible/init.sh
@@ -4,6 +4,14 @@ echo "⚠️ ================================================"
 echo "START: Ansible init stage"
 
 echo "TODO"
+if ! command -v ansible >/dev/null 2>&1; then
+  echo "❌ Error: 'ansible' command not found. Please install Ansible before proceeding."
+fi
+
+if ! command -v ansible-playbook >/dev/null 2>&1; then
+  echo "❌ Error: 'ansible-playbook' command not found. Please install Ansible before proceeding."
+fi
+
 EXIT_CODE=0
 
 echo "⚠️ ================================================"
