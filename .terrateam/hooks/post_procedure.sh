48f97b24887c3d2876ca1cc5348923b92f8d7c89
Ryszard Styczynski <rstyczynski@gmail.com>
2025-08-21 11:21:09 +0200

    .

--- .terrateam/hooks/post_procedure.sh
+++ .terrateam/hooks/post_procedure.sh
@@ -3,8 +3,9 @@
 echo "⚠️ ================================================" >&2
 echo "START: post-hook" >&2
 
-echo ">>TERRATEAM_PLAN_FILE: $TERRATEAM_PLAN_FILE"
+echo ">>TERRATEAM_PLAN_FILE: $TERRATEAM_PLAN_FILE" >&2
 cat $TERRATEAM_PLAN_FILE >&2
-echo "<<TERRATEAM_PLAN_FILE"
+echo "<<TERRATEAM_PLAN_FILE" >&2
+
 echo "END: post-hook" >&2
 echo "⚠️ ================================================" >&2
