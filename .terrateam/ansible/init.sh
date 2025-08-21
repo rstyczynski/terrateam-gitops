5af368dd19b3f2c814cdc4cfbb382ae17445f72a
Ryszard Styczynski <rstyczynski@gmail.com>
2025-08-21 09:18:06 +0200

    missing ansible notice

--- .terrateam/ansible/init.sh
+++ .terrateam/ansible/init.sh
@@ -4,16 +4,16 @@ echo "⚠️ ================================================"
 echo "START: Ansible init stage"
 
 echo "TODO"
-if ! command -v ansible >/dev/null 2>&1; then
-  echo "❌ Error: 'ansible' command not found. Please install Ansible before proceeding."
-else
-    ansible --version
-fi
-
 if ! command -v ansible-playbook >/dev/null 2>&1; then
   echo "❌ Error: 'ansible-playbook' command not found. Please install Ansible before proceeding."
 else
     ansible-playbook --version
+
+    if ! command -v ansible >/dev/null 2>&1; then
+        echo "⚠️ Notice: 'ansible' command not found. Please install Ansible before proceeding."
+    else
+        ansible --version
+    fi
 fi
 
 EXIT_CODE=0
