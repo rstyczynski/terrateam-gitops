85f410b47b712d700b6e0fc08f96cb26d9bbf07b
Ryszard Styczynski <rstyczynski@gmail.com>
2025-09-19 10:41:29 +0200

    .

--- .terrateam/debug/init.sh
+++ .terrateam/debug/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "⚠️ ================================================" >&2
-echo "START: Ansible init stage" >&2
+echo "START: Debug init stage" >&2
 
 echo "TODO Ansible init"
 
@@ -11,5 +11,5 @@ source "$(dirname "$0")/../shared/debug.sh" >&2
 EXIT_CODE=0
 
 echo "⚠️ ================================================" >&2
-echo "STOP: Ansible init stage" >&2
+echo "STOP: Debug init stage" >&2
 exit $EXIT_CODE
--- .terrateam/debug/outputs.sh
+++ .terrateam/debug/outputs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "⚠️ ================================================" >&2
-echo "START: Ansible outputs stage" >&2
+echo "START: Debug outputs stage" >&2
 
 source "$(dirname "$0")/../shared/debug.sh" >&2
 
 echo '{"status": "success", "stage": "outputs", "message": "Outputs stage completed successfully."}'
 EXIT_CODE=0
 
-echo "END: Ansible outputs stage" >&2
+echo "END: Debug outputs stage" >&2
 echo "⚠️ ================================================" >&2
 exit $EXIT_CODE
 
