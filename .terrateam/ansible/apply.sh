a6aa50e36451ac7b968f1840edbc46e0bc2d66d0
Ryszard Styczynski <rstyczynski@gmail.com>
2025-09-19 08:02:28 +0200

    .

--- .terrateam/ansible/apply.sh
+++ .terrateam/ansible/apply.sh
@@ -6,6 +6,7 @@ echo "START: Ansible apply stage" >&2
 source "$(dirname "$0")/../shared/debug.sh" >&2
 
 echo "TODO: Ansible apply"
+echo "CWD: $PWD"
 EXIT_CODE=0
 
 echo "END: Ansible apply stage" >&2
