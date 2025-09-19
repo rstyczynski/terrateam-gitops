e4b52f339f0fa6bad939a03131f02163b1e99a39
Ryszard Styczynski <rstyczynski@gmail.com>
2025-09-19 11:08:13 +0200

    .

--- .terrateam/ansible/init.sh
+++ .terrateam/ansible/init.sh
@@ -3,7 +3,38 @@
 echo "⚠️ ================================================" >&2
 echo "START: Ansible init stage" >&2
 
-echo "TODO Ansible init"
+echo "Ansible init"
+
+#
+# detect workspace 
+#
+if [ "${TERRATEAM_WORKSPACE}" == "default" ]; then
+    ANSIBLE_ROOT=${TERRATEAM_ROOT}/${TERRATEAM_DIR}
+else
+    ANSIBLE_ROOT=${TERRATEAM_ROOT}/${TERRATEAM_DIR}/${TERRATEAM_WORKSPACE}
+fi
+cd $ANSIBLE_ROOT
+
+#
+# detest ansible.cfg
+#
+if [ -f "ansible.cfg" ]; then
+    echo "Ansible cfg file found"
+    cat ansible.cfg
+else
+    echo "Using default ansible configuration.ansible.cfg file not found in workspace directory."
+fi
+
+#
+# install requirements
+#
+if [ -f "requirements.yml" ]; then
+    echo "Requirements file found"
+    ansible-galaxy install -r requirements.yml
+else
+    echo "No requirements to install. Requirements file not found in workspace directory."
+fi
+
 
 
 EXIT_CODE=0
