a86d94c5c70be5c50009d34fd12c5acab96b6224
Ryszard Styczynski <rstyczynski@gmail.com>
2025-09-19 08:27:57 +0200

    .

--- .terrateam/ansible/diff.sh
+++ .terrateam/ansible/diff.sh
@@ -10,6 +10,9 @@ source "$(dirname "$0")/../shared/debug.sh" >&2
 echo "TODO Ansible diff stdout message. Here is the content of $TERRATEAM_PLAN_FILE prepared at plan stage:"
 cat $TERRATEAM_PLAN_FILE
 echo "CWD: $PWD"
+echo "TERRATEAM_DIR: $TERRATEAM_DIR"
+echo "TERRATEAM_WORKSPACE: $TERRATEAM_WORKSPACE"
+echo "TERRATEAM_ROOT: $TERRATEAM_ROOT"
 
 EXIT_CODE=0
 
