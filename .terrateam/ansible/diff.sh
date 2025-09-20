b668f26388a4f4a7e825922176df06012e41a06a
Ryszard Styczynski <rstyczynski@gmail.com>
2025-09-20 19:11:29 +0200

    Refactor debug output in diff.sh to be conditional based on TERRATEAM_DEBUG variable

--- .terrateam/ansible/diff.sh
+++ .terrateam/ansible/diff.sh
@@ -7,43 +7,43 @@ echo "START: Ansible diff stage" >&2
 
 echo "Ansible will be executed in the following context:"
 cat $TERRATEAM_PLAN_FILE
-
-
-echo 
-echo 
-echo "Environment variables (DEBUG):"
-echo "=============================="
-echo "CWD: $PWD"
-echo "TERRATEAM_DIR: $TERRATEAM_DIR"
-echo "TERRATEAM_WORKSPACE: $TERRATEAM_WORKSPACE"
-echo "TERRATEAM_ROOT: $TERRATEAM_ROOT"
-
-echo 
-echo 
-echo "Other tests (DEBUG):"
-echo "=============================="
-which ansible-galaxy
-which yq
-which jq
-
-json=$(python3 -c 'import sys,yaml,json; print(json.dumps(yaml.safe_load(open(sys.argv[1]))))' $TERRATEAM_PLAN_FILE)
-echo "$json" | jq
-
-PLAYBOOK=$(echo "$json" | jq -r '.ansible_execution_context.ANSIBLE_PLAYBOOK')
-echo "PLAYBOOK: $PLAYBOOK"
-ROOT=$(echo "$json" | jq -r '.ansible_execution_context.ENV.TERRATEAM_ROOT')
-echo "ROOT: $ROOT"
-DIR=$(echo "$json" | jq -r '.ansible_execution_context.ENV.TERRATEAM_DIR')
-echo "DIR: $DIR"
-WORKSPACE=$(echo "$json" | jq -r '.ansible_execution_context.ENV.TERRATEAM_WORKSPACE')
-echo "WORKSPACE: $WORKSPACE"
-ANSIBLE_ROOT=$(echo "$json" | jq -r '.ansible_execution_context.ENV.ANSIBLE_ROOT')
-echo "ANSIBLE_ROOT: $ANSIBLE_ROOT"
-
 EXIT_CODE=0
 
+
 TERRATEAM_DEBUG=false
-source "$(dirname "$0")/../shared/debug.sh" >&2
+if [ "${TERRATEAM_DEBUG}" == "true" ]; then
+    echo 
+    echo 
+    echo "Environment variables (DEBUG):"
+    echo "=============================="
+    echo "CWD: $PWD"
+    echo "TERRATEAM_DIR: $TERRATEAM_DIR"
+    echo "TERRATEAM_WORKSPACE: $TERRATEAM_WORKSPACE"
+    echo "TERRATEAM_ROOT: $TERRATEAM_ROOT"
+
+    echo 
+    echo 
+    echo "Other tests (DEBUG):"
+    echo "=============================="
+    which ansible-galaxy
+    which yq
+    which jq
+
+    json=$(python3 -c 'import sys,yaml,json; print(json.dumps(yaml.safe_load(open(sys.argv[1]))))' $TERRATEAM_PLAN_FILE)
+    echo "$json" | jq
+
+    PLAYBOOK=$(echo "$json" | jq -r '.ansible_execution_context.ANSIBLE_PLAYBOOK')
+    echo "PLAYBOOK: $PLAYBOOK"
+    ROOT=$(echo "$json" | jq -r '.ansible_execution_context.ENV.TERRATEAM_ROOT')
+    echo "ROOT: $ROOT"
+    DIR=$(echo "$json" | jq -r '.ansible_execution_context.ENV.TERRATEAM_DIR')
+    echo "DIR: $DIR"
+    WORKSPACE=$(echo "$json" | jq -r '.ansible_execution_context.ENV.TERRATEAM_WORKSPACE')
+    echo "WORKSPACE: $WORKSPACE"
+    ANSIBLE_ROOT=$(echo "$json" | jq -r '.ansible_execution_context.ENV.ANSIBLE_ROOT')
+    echo "ANSIBLE_ROOT: $ANSIBLE_ROOT"
+
+fi
 
 echo "END: Ansible diff stage" >&2
 echo "⚠️ ================================================" >&2
