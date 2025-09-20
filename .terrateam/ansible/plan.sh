1094fd9b11833961cc7f31e1caa42ee5d555122d
Ryszard Styczynski <rstyczynski@gmail.com>
2025-09-20 23:29:58 +0200

    Enable debug mode in plan.sh and update version in vars.json from 1.0.13 to 1.0.14

--- .terrateam/ansible/plan.sh
+++ .terrateam/ansible/plan.sh
@@ -208,7 +208,7 @@ echo "TERRATEAM_PLAN_FILE (exported): $TERRATEAM_PLAN_FILE" >&2
 echo "TERRATEAM_PLAN_FILE (argumnet): $PLAN_FILE" >&2
 EXIT_CODE=0
 
-TERRATEAM_DEBUG=false
+TERRATEAM_DEBUG=true
 source "$(dirname "$0")/../shared/debug.sh" >&2
 
 echo "END: Ansible plan stage" >&2
